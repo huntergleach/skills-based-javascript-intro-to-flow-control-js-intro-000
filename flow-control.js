b6283ec730ad4576a9706caeb60652e4433e771f
Hunter Leach <hunterleach94@gmail.com>
2021-05-10 22:15:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,14 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19)
+   return "You are a teenager!"
 }
 
 function teenager(age) {
-
+if (age >= 13 && age <= 19) {
+  return "You are a teenager!"
+} else {
+  return "You are not a teenager"
+}
 }
 
 function ageChecker(age) {
@@ -11,7 +16,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+return (age <= 19 && age >=13) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
